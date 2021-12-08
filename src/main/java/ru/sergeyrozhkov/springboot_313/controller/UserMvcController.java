fe919962c5b3b10a33d967df4ad4aec4d748ef0a
Serge Rozkov <rozkov_ss@mail.ru>
2021-12-08 08:47:10 +0300

    savepoint

--- src/main/java/ru/sergeyrozhkov/springboot_313/controller/UserMvcController.java
+++ src/main/java/ru/sergeyrozhkov/springboot_313/controller/UserMvcController.java
@@ -1,13 +1,30 @@
 package ru.sergeyrozhkov.springboot_313.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.sergeyrozhkov.springboot_313.service.UserService;
+
+import java.security.Principal;
 
 @Controller
 public class UserMvcController {
+    private UserService userService;
+
+    @Autowired
+    public UserMvcController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public String getIndex() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String showUserPage(Principal principal, Model model) {
+        model.addAttribute(userService.findUserByEmail(principal.getName()));
+        return "user";
     }
 }
